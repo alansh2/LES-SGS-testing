e27d8595df5ec4071909ab847bb10fd345586fdf
alansh2 <56527388+alansh2@users.noreply.github.com>
2021-07-08 13:28:49 -0500

    added 1 plotting line + repc in tensor for speed

--- testLES_SGS.m
+++ testLES_SGS.m
@@ -1,17 +1,5 @@
 % testLES_SGS.m - LES SGS Model Test From DNS
-%
-%     y
-%     |  ,----------------------,
-%     | /|                     /|
-%     |/ |                    / |
-%     2--+-------------------:  |
-%     |  | z                 |  |
-%     |  |/                  |  |
-% 512 | 3*pi-----------------+--;
-%     | /                    | / 1536
-%     |/                     |/
-%     +---------------------8*pi--->x
-%               2048
+% 
 % 
 
 close all
@@ -20,15 +8,15 @@ clear all
 % load DNS
 dat = matfile('data.mat');
 
-% suppose we have 'u' extracted [U; V; W]
-% assume single frame data is temporal average
 % SGS stress tensor = <uiuj> - <ui><uj>
 % <.> = filtering operation
+% Tij = 
 
+% Gaussian filter
 Gstd = 0.5; % half channel half height
-u1 = imgaussfilt3(dat.U,Gstd);
-u2 = imgaussfilt3(dat.V,Gstd);
-u3 = imgaussfilt3(dat.W,Gstd);
+Gu1 = imgaussfilt3(dat.U,Gstd);
+Gu2 = imgaussfilt3(dat.V,Gstd);
+Gu3 = imgaussfilt3(dat.W,Gstd);
 
 % specify grid widths
 [ny, nx, nz] = size(dat,'U');
@@ -36,9 +24,9 @@ hx = 8*pi/(nx-1);
 hy = 2/(ny-1);
 hz = 3*pi/(nz-1);
 % size(dat.VAR) = ny x nx x nz
-[dUdy,dUdx,dUdz] = gradient(u1,hy,hx,hz);
-[dVdy,dVdx,dVdz] = gradient(u2,hy,hx,hz);
-[dWdy,dWdx,dWdz] = gradient(u3,hy,hx,hz);
+[dUdy,dUdx,dUdz] = gradient(Gu1,hy,hx,hz);
+[dVdy,dVdx,dVdz] = gradient(Gu2,hy,hx,hz);
+[dWdy,dWdx,dWdz] = gradient(Gu3,hy,hx,hz);
 
 S11 = dUdx;
 S12 = (dUdy + dVdx)/2;
@@ -54,9 +42,15 @@ Delta = (hx*hy*hz)^(1/3);
 cs = 0.16;
 
 % stress tensor
-T11 = -2*(cs*Delta)^2*Snorm.*S11;
-T12 = -2*(cs*Delta)^2*Snorm.*S12;
-T13 = -2*(cs*Delta)^2*Snorm.*S13;
-T22 = -2*(cs*Delta)^2*Snorm.*S22;
-T23 = -2*(cs*Delta)^2*Snorm.*S23;
-T33 = -2*(cs*Delta)^2*Snorm.*S33;
+repc = -2*(cs*Delta)^2;
+T11 = repc*Snorm.*S11;
+T12 = repc*Snorm.*S12;
+T13 = repc*Snorm.*S13;
+T22 = repc*Snorm.*S22;
+T23 = repc*Snorm.*S23;
+T33 = repc*Snorm.*S33;
+
+% plotting
+figure(1)
+contourf(dat.X(:,:,1), dat.Y(:,:,1), T12(:,:,1), 30, 'LineStyle', 'none');
+colorbar
