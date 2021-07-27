16167ba6508d0491e22dc39792132efc76352114
alansh2 <56527388+alansh2@users.noreply.github.com>
2021-07-27 18:50:25 -0500

    minor performance optimizations

--- Smagorinsky.m
+++ Smagorinsky.m
@@ -1,8 +1,8 @@
 function ev = Smagorinsky(Delta,S);
     % Returns kinematic eddy viscosity
     % calculate invariant
-    Snorm = sqrt(2*(S.S11.*S.S11 + 2*S.S12.*S.S12 + 2*S.S13.*S.S13 ...
-        + S.S22.*S.S22 + 2*S.S23.*S.S23 + S.S33.*S.S33));
+    Snorm = sqrt(2*(S.S11.^2 + 2*S.S12.^2 + 2*S.S13.^2 ...
+        + S.S22.^2 + 2*S.S23.^2 + S.S33.^2));
 
     cs = 0.16; % Smagorinsky coefficient
     ev = -2*(cs*Delta)^2*Snorm; % eddy viscosity
